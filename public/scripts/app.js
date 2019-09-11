264050abf2dbd295d13ad2e88811645180da160e
Julien Atanassian <julien.atanassian@gmail.com>
2019-09-11 01:13:29 +0000

    implement the createTweetElement function that takes the data (object) of a tweet and returns the html code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,52 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+ // Test / driver code (temporary). Eventually will get this from the server.
+ const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+$(document).ready(function() {
+
+  const createTweetElement = function(tweetObj) {
+    const element = `
+      <article class="tweet">
+      <header>
+        <img src=${tweetObj.user.avatars} />
+        ${tweetObj.user.name}
+        <span class="handle">${tweetObj.user.handle}</span>
+      </header>
+      <div class="content">
+          ${tweetObj.content.text}
+      </div>
+      <footer>
+        <span class="date">
+        ${tweetObj.created_at}
+        </span>
+        <div class="actions">
+          <img src="/images/flag.png">
+          <img src="/images/retweet-symbol.png">
+          <img src="/images/like-symbol.png">
+        </div>
+      </footer>
+      </article>
+    `;
+    return element;
+  }
+
+  const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+});
